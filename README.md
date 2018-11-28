e1aeb87ad79f850d4bbe4b9b8e40b9ee5e7d8e9a
marshal <marshal.wu@qq.com>
2018-11-28 16:53:43 +0800

    finish how_to_get_widget_height

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ## 版本分支
 
+* `how_to_get_widget_height`，获得任意widget的尺寸和高度
 * `hello_world`，最简单的示例，将Android studio自动生成的项目做了再次简化
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// 演示，使用globalKey，获取任意Widget的尺寸和位置
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -18,15 +20,43 @@ class AppPage extends StatefulWidget {
 
 class _AppPageState extends State<AppPage> {
 
+  GlobalKey _keyAmber = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
       ),
-      body: Center(
-        child: Text('Hello world.'),
-      ),
+      body: Column(
+        children: <Widget>[
+          GestureDetector(
+            child: Container(
+              key: _keyAmber,
+              color: Colors.amber,
+              constraints: BoxConstraints(
+                  minHeight: 100
+              ),
+            ),
+            onTap: (){
+              final RenderBox box = _keyAmber.currentContext.findRenderObject();
+              print('Height is ${box.size.height}, position is ${box.localToGlobal(Offset.zero)}');
+            },
+          ),
+          Container(
+            color: Colors.green,
+            constraints: BoxConstraints(
+              minHeight: 200
+            ),
+          ),
+          Container(
+            color: Colors.cyan,
+            constraints: BoxConstraints(
+                minHeight: 120
+            ),
+          )
+        ],
+      )
     );
   }
 }
