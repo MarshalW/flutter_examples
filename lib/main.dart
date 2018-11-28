8ef0044fcd58e0918bbadad60d4e3f2cb9f507e7
marshal <marshal.wu@qq.com>
2018-11-28 21:07:28 +0800

    basic complete but need refector

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,101 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new AppContainer(child: new MyApp()));
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AppPage(),
+      home: Center(
+        child: HomePage(),
+      ),
     );
   }
 }
 
-class AppPage extends StatefulWidget {
+class HomePage extends StatefulWidget {
+  User user;
+
   @override
-  _AppPageState createState() => _AppPageState();
+  HomePageState createState() => new HomePageState();
 }
 
-class _AppPageState extends State<AppPage> {
+class HomePageState extends State<HomePage> {
+  User user;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: Center(
-        child: Text('Hello world.'),
-      ),
+    final container = AppContainer.of(context);
+    user = container.user;
+    debugPrint('user: $user');
+    return new Scaffold(
+        appBar: AppBar(
+          title: Text("Home"),
+        ),
+        body: Center(
+          child: Text('Hello world, ${user.name}'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.edit),
+          onPressed: () {
+            container.updateUser(name:'Hoke');
+          },
+        ));
+  }
+}
+
+class AppContainer extends StatefulWidget {
+  final Widget child;
+  final User user;
+
+  AppContainer({
+    @required this.child,
+    this.user,
+  });
+
+  @override
+  _AppContainerState createState() => _AppContainerState();
+
+  static _AppContainerState of(BuildContext context) {
+    return (context.inheritFromWidgetOfExactType(_InheritedStateWidget)
+            as _InheritedStateWidget)
+        .data;
+  }
+}
+
+class _AppContainerState extends State<AppContainer> {
+  User user = new User('Joe');
+
+  @override
+  Widget build(BuildContext context) {
+    return new _InheritedStateWidget(
+      data: this,
+      child: widget.child,
     );
   }
+
+  void updateUser({name}) {
+    setState(() {
+      user.name = name;
+    });
+  }
+}
+
+class User {
+  String name;
+
+  User(this.name);
+}
+
+class _InheritedStateWidget extends InheritedWidget {
+  final _AppContainerState data;
+
+  _InheritedStateWidget({
+    Key key,
+    @required this.data,
+    @required Widget child,
+  }) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(_InheritedStateWidget old) => true;
 }
