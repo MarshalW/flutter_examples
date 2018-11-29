055981e844650473e3c21c0aacd057f92dfcbbf6
marshal <marshal.wu@qq.com>
2018-11-29 14:13:20 +0800

    finished simple inherited demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,41 @@
+// 本文示例来源：https://loveky.github.io/2018/07/18/how-flutter-inheritedwidget-works/
+// 这是我目前见到最简明的示例
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyWelcomeInfo extends InheritedWidget {
+  MyWelcomeInfo({Key key, this.welcomeInfo, Widget child})
+      : super(key: key, child: child);
+
+  final String welcomeInfo;
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: AppPage(),
-    );
+  bool updateShouldNotify(MyWelcomeInfo oldWidget) {
+    return oldWidget.welcomeInfo != welcomeInfo;
   }
 }
 
-class AppPage extends StatefulWidget {
+class MyNestedChild extends StatelessWidget {
   @override
-  _AppPageState createState() => _AppPageState();
+  build(BuildContext context) {
+    final MyWelcomeInfo widget =
+    context.inheritFromWidgetOfExactType(MyWelcomeInfo);
+    return Text(widget.welcomeInfo);
+  }
 }
 
-class _AppPageState extends State<AppPage> {
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: Center(
-        child: Text('Hello world.'),
-      ),
+    return new MaterialApp(
+      title: 'Flutter InheritWidget',
+      home: MyWelcomeInfo(
+          welcomeInfo: 'hello flutter',
+          child: Center(
+            child: MyNestedChild(),
+          )),
     );
   }
 }
