829c0814e5982ca7f1d17a162ca82ed10aeeea3e
marshal <marshal.wu@qq.com>
2018-11-29 19:23:45 +0800

    add comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
+// InheritedModel比较新，2018-9才出来，目前示例很少
+// 可参见：https://gist.github.com/HansMuller/b947e76c34d941397989eaab537d45ca
+// 初步认识是对InheritedWidget的细化，InheritedWidget不能对模型的局部改变做处理，只能无条件的rebuild
+// InheritedModel提供了基于aspect的细粒度处理机制
+// TODO 待实现
+
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
