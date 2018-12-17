2885404b76a1efc2c15e153745e9091efd2b7d62
marshal <marshal.wu@qq.com>
2018-12-17 15:14:14 +0800

    finished homework

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,12 @@
-import 'package:flutter/material.dart';
+// https://github.com/flutter/flutter/blob/master/examples/layers/rendering/hello_world.dart
+// 学习怎样在渲染树绘制文字内容
 
-void main() => runApp(MyApp());
+import 'package:flutter/rendering.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: AppPage(),
-    );
-  }
-}
-
-class AppPage extends StatefulWidget {
-  @override
-  _AppPageState createState() => _AppPageState();
-}
-
-class _AppPageState extends State<AppPage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: Center(
-        child: Text('Hello world.'),
-      ),
-    );
-  }
+void main() {
+  RenderingFlutterBinding(
+      root: RenderPositionedBox(
+          alignment: Alignment.center,
+          child: RenderParagraph(TextSpan(text: '你好，世界！'),
+              textDirection: TextDirection.ltr)));
 }
