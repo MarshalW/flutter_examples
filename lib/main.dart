93957a3709a4b3309e23b25fba39c59ae510141f
marshal <marshal.wu@qq.com>
2018-12-17 16:41:32 +0800

    finished homework

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,37 @@
-import 'package:flutter/material.dart';
+// https://github.com/flutter/flutter/blob/master/examples/layers/widgets/custom_render_box.dart
+// 简化，去除touch相关逻辑，只实现自定义的paint
 
-void main() => runApp(MyApp());
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
-class MyApp extends StatelessWidget {
+class Dots extends SingleChildRenderObjectWidget {
+  const Dots({Key key, Widget child}) : super(key: key, child: child);
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: AppPage(),
-    );
+  RenderObject createRenderObject(BuildContext context) {
+    return RenderDots();
   }
 }
 
-class AppPage extends StatefulWidget {
+class RenderDots extends RenderConstrainedBox {
+  RenderDots() : super(additionalConstraints: BoxConstraints.expand());
+
   @override
-  _AppPageState createState() => _AppPageState();
+  void paint(PaintingContext context, Offset offset) {
+    Canvas canvas = context.canvas;
+
+    Paint paint = Paint()..color = const Color(0xff1a237e);
+    canvas.drawCircle(Offset(200, 450), 100.0, paint);
+    super.paint(context, offset);
+  }
 }
 
-class _AppPageState extends State<AppPage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: Center(
-        child: Text('Hello world.'),
-      ),
-    );
-  }
+void main() {
+  runApp(Directionality(
+      textDirection: TextDirection.ltr,
+      child: Dots(
+        child: Center(
+          child: Text('你好，世界！'),
+        ),
+      )));
 }
