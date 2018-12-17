151fde425cd7b62092736f78c19e59c7df630fc0
marshal <marshal.wu@qq.com>
2018-12-17 15:02:23 +0800

    finished homework

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,38 @@
-import 'package:flutter/material.dart';
+//https://github.com/flutter/flutter/blob/master/examples/layers/raw/hello_world.dart
+//基本照搬flutter的示例，学习怎么使用最底层的api编写界面
 
-void main() => runApp(MyApp());
+import 'dart:ui';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: AppPage(),
-    );
-  }
+void beginFrame(Duration timeStamp) {
+  double devicePixelRatio = window.devicePixelRatio;
+  Size logicalSize = window.physicalSize / devicePixelRatio;
+
+  ParagraphBuilder paragraphBuilder =
+      ParagraphBuilder(ParagraphStyle(textDirection: TextDirection.ltr))
+        ..addText('你好, 世界！');
+
+  Paragraph paragraph = paragraphBuilder.build()
+    ..layout(ParagraphConstraints(width: logicalSize.width));
+
+  Rect physicalBounds = Offset.zero & (logicalSize * devicePixelRatio);
+  PictureRecorder recorder = PictureRecorder();
+  Canvas canvas = Canvas(recorder, physicalBounds);
+  canvas.scale(devicePixelRatio, devicePixelRatio);
+  canvas.drawParagraph(
+      paragraph,
+      Offset((logicalSize.width - paragraph.maxIntrinsicWidth) / 2.0,
+          (logicalSize.height - paragraph.height) / 2.0));
+  Picture picture=recorder.endRecording();
+
+  SceneBuilder sceneBuilder=SceneBuilder()
+  ..pushClipRect(physicalBounds)
+  ..addPicture(Offset.zero, picture)
+  ..pop();
+
+  window.render(sceneBuilder.build());
 }
 
-class AppPage extends StatefulWidget {
-  @override
-  _AppPageState createState() => _AppPageState();
-}
-
-class _AppPageState extends State<AppPage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: Center(
-        child: Text('Hello world.'),
-      ),
-    );
-  }
+void main() {
+  window.onBeginFrame = beginFrame;
+  window.scheduleFrame();
 }
